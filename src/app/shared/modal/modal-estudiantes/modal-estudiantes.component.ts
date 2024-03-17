11f6f8486f9dc0644eb74ce8c55be4d60d848f3b
Luis Alvarez <alvalero34@gmil.com>
2024-03-17 01:02:36 -0400

    add- modal-estudiantesfuncionando

--- src/app/shared/modal/modal-estudiantes/modal-estudiantes.component.ts
+++ src/app/shared/modal/modal-estudiantes/modal-estudiantes.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbActiveModal, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { Router } from 'express';
+import { log } from 'console';
+
 
 @Component({
   selector: 'app-modal-estudiantes',
@@ -13,16 +15,36 @@ import { Router } from 'express';
 })
 export class ModalEstudiantesComponent {
   constructor(public activeModal: NgbActiveModal,private fb:FormBuilder, private router:Router) { }
+  tercero:any[]=[];
   formulario!: FormGroup;
 
   ngOnInit(): void {
   this.formInit();
+  let listaEstudiante = localStorage.getItem('logeado');
+  let segundo: any = JSON.parse(listaEstudiante!);
+  this.tercero= JSON.parse(segundo.estudiantes!);
+  console.log("imprime",this.tercero);
+  
 }
 
 formInit() {
   this.formulario= this.fb.group({
-    estudiante:new FormControl('',[Validators.required]),
+    estudiante:new FormControl(null,[Validators.required]),
+    cedula:new FormControl(null),
+    nombreYapellido:new FormControl(null),
+
   });
+  this.setdatos();
+}
+
+setdatos(){
+  this.formulario.get("estudiante")?.valueChanges.subscribe(value=>{
+    let item=this.tercero[value];
+     this.formulario.get("cedula")?.setValue(item.cedula);
+ this.formulario.get("nombreYapellido")?.setValue(item.nombreYapellido);
+  })
+
+  
 }
 
 }
--- src/app/view/interfaz-profesor/interfaz-estudiantes-profesor/interfaz-estudiantes-profesor.component.ts
+++ src/app/view/interfaz-profesor/interfaz-estudiantes-profesor/interfaz-estudiantes-profesor.component.ts
@@ -1,39 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuVerticalProfesorComponent } from '../../layout/menu-vertical-profesor/menu-vertical-profesor.component';
-import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateUserModalComponent } from '../../../shared/modal/create-user-modal/create-user-modal.component';
 import { InterfazEvaluacionesProfesorComponent } from '../interfaz-evaluaciones-profesor/interfaz-evaluaciones-profesor.component';
 import { CommonModule } from '@angular/common';
+import { ModalEstudiantesComponent } from '../../../shared/modal/modal-estudiantes/modal-estudiantes.component';
 
 @Component({
   selector: 'app-interfaz-estudiantes-profesor',
   standalone: true,
-  imports: [MenuVerticalProfesorComponent,
+  imports: [
+    MenuVerticalProfesorComponent,
     NgbDatepickerModule,
+    ModalEstudiantesComponent,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
     CreateUserModalComponent,
-    InterfazEvaluacionesProfesorComponent
+    InterfazEvaluacionesProfesorComponent,
   ],
   templateUrl: './interfaz-estudiantes-profesor.component.html',
-  styleUrl: './interfaz-estudiantes-profesor.component.css'
+  styleUrl: './interfaz-estudiantes-profesor.component.css',
 })
-export class InterfazEstudiantesProfesorComponent implements OnInit{
-  relleno:any[]=[];
+export class InterfazEstudiantesProfesorComponent implements OnInit {
+  relleno: any[] = [];
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {
-  let listaEstudiante= localStorage.getItem("logeado");
-  let segundo:any = JSON.parse(listaEstudiante!);
-  let tercero:any[] = JSON.parse(segundo.estudiantes!);
-  console.log("Estudiantes,",tercero);  
-tercero.forEach((value, index)=>{
-  this.relleno.push({
-    indice:index+1,
-    cedula:value.cedula,
-    nombreYapellido:value.nombreYapellido,
-  });
-});
-  
-}}
+    let listaEstudiante = localStorage.getItem('logeado');
+    let segundo: any = JSON.parse(listaEstudiante!);
+    let tercero: any[] = JSON.parse(segundo.estudiantes!);
+    console.log('Estudiantes,', tercero);
+    tercero.forEach((value, index) => {
+      this.relleno.push({
+        indice: index + 1,
+        cedula: value.cedula,
+        nombreYapellido: value.nombreYapellido,
+      });
+    });
+  }
+  open() {
+    const modalRef = this.modalService.open(ModalEstudiantesComponent);
+    modalRef.result.then((value) => {
+      console.log('text', value);
+    });
+  }
+}
